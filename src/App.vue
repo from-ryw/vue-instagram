a47ce4a81b93f285099149706c49ede12cf9efa5
from-ryw <from.ryw@gmail.com>
2025-04-08 19:03:04 +0900

    fix: String 타입 초기값을 ''로 수정하고 필터 선택 시 오류 해결

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,63 @@
 <!-- script -->
 <script setup>
-import axios from 'axios';
-import { onMounted, onUnmounted, ref } from 'vue';
-import Container from './components/Container.vue';
-import posts from './assets/posts';
-import filters from './assets/filters';
+import axios from 'axios'
+import { onMounted, onUnmounted, ref } from 'vue'
+import Container from './components/Container.vue'
+import posts from './assets/posts'
+import filters from './assets/filters'
 
-const postList = ref([...posts]); // 화면에서 사용하는 반응형 리스트
+const postList = ref([...posts]) // 화면에서 사용하는 반응형 리스트
 
-let lastScrollTop = 0;            // 이전 스크롤 위치 기억
-const isLoading = ref(false);     // 중복 실행 방지용 플래그
-const count = ref(0);             // GET요청 시, 사용하는 count(more1까지만 요청이 가능)
-const realCount = ref(1);         // 실제 count
+let lastScrollTop = 0 // 이전 스크롤 위치 기억
+const isLoading = ref(false) // 중복 실행 방지용 플래그
+const count = ref(0) // GET요청 시, 사용하는 count(more1까지만 요청이 가능)
+const realCount = ref(1) // 실제 count
 
-const step = ref(0);                // 단계
-const uploadFileURL = ref(null);    // 업로드 파일 이미지 주소
-const uploadFileFilter = ref(null); // 업로드 파일 필터
-const uploadContent = ref(null);    // 업로드 파일 내용
-
-
+const step = ref(0) // 단계
+const uploadFileURL = ref('') // 업로드 파일 이미지 주소
+const uploadFileFilter = ref('') // 업로드 파일 필터
+const uploadContent = ref('') // 업로드 파일 내용
 
 // 포스트 더보기
 async function loadMorePosts() {
-  if (isLoading.value) return; // 이미 실행 중이면 무시
+  if (isLoading.value) return // 이미 실행 중이면 무시
 
-  isLoading.value = true; // 실행 시작
+  isLoading.value = true // 실행 시작
   try {
-    const result = await axios.get(`https://codingapple1.github.io/vue/more${count.value}.json`);
-    
+    const result = await axios.get(`https://codingapple1.github.io/vue/more${count.value}.json`)
+
     // postImage 랜덤으로 바꾸기
-    result.data.postImage =  "https://picsum.photos/600?random=" + Math.floor(Math.random() * 100);
+    result.data.postImage = 'https://picsum.photos/600?random=' + Math.floor(Math.random() * 100)
 
     // filter 랜덤으로 바꾸기
-    result.data.filter = filters[Math.floor(Math.random() * filters.length)];
+    result.data.filter = filters[Math.floor(Math.random() * filters.length)]
 
     // content 숫자 바꾸기
-    result.data.content = result.data.content.replace(/\d+/, realCount.value);
-    postList.value.push(result.data);
+    result.data.content = result.data.content.replace(/\d+/, realCount.value)
+    postList.value.push(result.data)
 
     // 카운트 업데이트
-    realCount.value++;
-    count.value = (count.value + 1) % 2; // 0 → 1 → 0 반복(GET요청이 1까지만 가능하므로 변경)
-
+    realCount.value++
+    count.value = (count.value + 1) % 2 // 0 → 1 → 0 반복(GET요청이 1까지만 가능하므로 변경)
   } catch (err) {
-    console.log(err.message);
-    console.log('실패');
-
+    console.log(err.message)
+    console.log('실패')
   } finally {
-    isLoading.value = false; // 완료되면 다시 실행 가능하게 설정
-
+    isLoading.value = false // 완료되면 다시 실행 가능하게 설정
   }
 }
 
 // 스크롤 감지 함수(상태바가 하단에 닿으면 loadMorePosts() 실행)
 function handleScroll() {
-  const scrollTop = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const documentHeight = document.documentElement.scrollHeight;
+  const scrollTop = window.scrollY
+  const windowHeight = window.innerHeight
+  const documentHeight = document.documentElement.scrollHeight
 
-  if (scrollTop > lastScrollTop) { // 아래로 스크롤인 경우
-    if (scrollTop + windowHeight >= documentHeight) { // 바닥 근처 인지 확인
-      loadMorePosts();
+  if (scrollTop > lastScrollTop) {
+    // 아래로 스크롤인 경우
+    if (scrollTop + windowHeight >= documentHeight) {
+      // 바닥 근처 인지 확인
+      loadMorePosts()
     }
   }
 
@@ -70,65 +67,69 @@ function handleScroll() {
 
 // 스크롤 이벤트 등록/해제
 onMounted(() => {
-  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('scroll', handleScroll)
 })
 onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll);
+  window.removeEventListener('scroll', handleScroll)
 })
 
 // like toggle 함수
 function handleToggleLike(index) {
-  const post = postList.value[index];
-  post.liked = !post.liked;
+  const post = postList.value[index]
+  post.liked = !post.liked
   if (post.liked) {
-    post.likes++;
+    post.likes++
   } else {
-    post.likes--;
+    post.likes--
   }
 }
 
 // Image upload 함수
 function uploadImage(e) {
-  let file = e.target.files;
-  let url = URL.createObjectURL(file[0]);
-  uploadFileURL.value = url;
-  step.value++;
+  let file = e.target.files
+  let url = URL.createObjectURL(file[0])
+  uploadFileURL.value = url
+  uploadFileFilter.value = ''
+  step.value++
 }
 
 // filter select 함수
 function handleSelectFilter(filterName) {
-  uploadFileFilter.value = filterName;
+  if (uploadFileFilter.value == filterName) {
+    uploadFileFilter.value = ''
+  } else {
+    uploadFileFilter.value = filterName
+  }
 }
 
 // content write 함수
 function handleWriteContent(content) {
-  uploadContent.value = content;
+  uploadContent.value = content
 }
 
 // Publish 함수
 function publish() {
   let myPost = {
-    name: "Ryu Yewon",
-    userImage: "https://picsum.photos/100?random=5",
+    name: 'Ryu Yewon',
+    userImage: 'https://picsum.photos/100?random=5',
     postImage: uploadFileURL.value,
     likes: 0,
     date: getToday(),
     liked: false,
     content: uploadContent.value,
-    filter: uploadFileFilter.value
-  };
-  postList.value.unshift(myPost);
-  step.value = 0;
+    filter: uploadFileFilter.value,
+  }
+  postList.value.unshift(myPost)
+  step.value = 0
 }
 
 // 오늘 날짜 함수
 function getToday() {
-  const today = new Date();
-  const options = { month: 'short', day: 'numeric' };
-  return today.toLocaleDateString('en-US', options);
+  const today = new Date()
+  const options = { month: 'short', day: 'numeric' }
+  return today.toLocaleDateString('en-US', options)
 }
 </script>
-
 
 <!-- template -->
 <template>
@@ -143,24 +144,25 @@ function getToday() {
     <img src="./assets/logo.svg" class="logo" />
   </div>
 
-  <Container :postList="postList" 
-             :filters="filters"
-             :step="step"
-             :uploadFileURL="uploadFileURL"
-             :uploadFileFilter="uploadFileFilter"
-             :uploadContent="uploadContent"
-             @toggle-like="handleToggleLike"
-             @select-filter="handleSelectFilter"
-             @write-content="handleWriteContent"/>
-            
+  <Container
+    :postList="postList"
+    :filters="filters"
+    :step="step"
+    :uploadFileURL="uploadFileURL"
+    :uploadFileFilter="uploadFileFilter"
+    :uploadContent="uploadContent"
+    @toggle-like="handleToggleLike"
+    @select-filter="handleSelectFilter"
+    @write-content="handleWriteContent"
+  />
+
   <div class="footer">
     <ul class="footer-button-plus" v-if="step == 0">
       <input type="file" id="file" class="inputfile" @change="uploadImage" />
       <label for="file" class="input-plus">+</label>
     </ul>
- </div>
+  </div>
 </template>
-
 
 <!-- style -->
 <style>
@@ -233,7 +235,7 @@ ul {
 }
 #app {
   box-sizing: border-box;
-  font-family: "consolas";
+  font-family: 'consolas';
   margin-top: 60px;
   width: 100%;
   max-width: 460px;
@@ -242,4 +244,4 @@ ul {
   border-right: 1px solid #eee;
   border-left: 1px solid #eee;
 }
-</style> 
+</style>
